b66d139e1ddeb1259acee38c805eeb9627d7e13e
Naheel Essam Qammaz <naheelqammaz@Naheels-MacBook-Pro.local>
2024-09-02 11:39:16 +0300

    terminate employee from amman branch-seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ employee3_Amman_termination= amman.employees.find_or_create_by(name:"employee 3 
 
 employee1_Dubai= dubai.employees.find_or_create_by(name:"employee 1 dubai ", employment_number: "1", email:"naheel4@zenhr.com", hiring_date:"2020-01-01")
 employee2_Dubai= dubai.employees.find_or_create_by(name:"employee 2 dubai ", employment_number: "2", email:"nahee5@zenhr.com", hiring_date:"2020-01-01")
-employee3_Dubai_termination= dubai.employees.find_or_create_by(name:"employee 3 dubai terminate", employment_number: "3", email:"naheel6@zenhr.com", hiring_date:"2020-01-01")
+employee3_Dubai= dubai.employees.find_or_create_by(name:"employee 3 dubai ", employment_number: "3", email:"naheje5@zenhr.com", hiring_date:"2020-01-01")
 
 
-
+Termination.find_or_create_by(employee: employee3_Amman_termination, date: "2024-12-31")
